157cb0e94b544f40f4cc13ad29867367292623ce
SValenziano <senorvalenz@gmail.com>
2026-04-13 14:48:51 -0400

    remove Solution code

--- solution/eod_day4/client/src/helpers/helpers.ts
+++
@@ -1,5 +0,0 @@
-import { CartItem } from "../types";
-
-export const calculateTotal = (cartItems: CartItem[]) => {
-  return cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
-};
